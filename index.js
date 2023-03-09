b2efc13377dcb87cdbf64f76a0c6f484d41bec5b
misya11p <miya33ps@gmail.com>
2023-03-09 14:58:45 +0900

    pattern ok

--- index.js
+++ index.js
@@ -14,9 +14,25 @@ function typing(event) {
     console.log(event.key);
     let key = event.key;
     if (key == futureText[0]) {
-        console.log('ok');
         inputText += key;
         futureText = futureText.slice(1);
+    } else if (key == 'c') {
+        if (futureText[0] == 's' || (futureText[0] == 'k' & futureText[1] != 'i')) {
+            inputText += key;
+            futureText = futureText.slice(1);
+        }
+    } else if (key == 'h') {
+        if (inputText.slice(-1)[0] == 's') {
+            inputText += key;
+        }
+    } else if (key == 'n') {
+        if (inputText.slice(-1)[0] == 'n' & inputText.slice(-2)[0] != 'n' & inputText.slice(-2)[0] != 'x') {
+            inputText += key;
+        }
+    } else if (key == 'x') {
+        if (futureText[0] == 'n') {
+            inputText += key;
+        }
     }
     progress_update();
 }
